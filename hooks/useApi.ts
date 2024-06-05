5da983d4c2615e4c194d7b87612b4a6e26b80005
gustavosilva7 <gustavosilvaalexgusta007@gmail.com>
2024-06-04 21:05:50 -0300

    chore: Update API base URL in useApi hook

--- hooks/useApi.ts
+++ hooks/useApi.ts
@@ -2,28 +2,28 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 const useApi = () => {
-    const api = axios.create({
-        baseURL: "http://localhost:8000/api"
-    });
+  const api = axios.create({
+    baseURL: "http://172.30.82.49:8181/api",
+  });
 
-    api.interceptors.request.use(async (config) => {
-        const token = await AsyncStorage.getItem("token");
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    });
-
-    const getUser = async () => {
-        try {
-            const response = await api.get("/user");
-            return response.data;
-        } catch (error) {
-            console.log(error);
-        }
+  api.interceptors.request.use(async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  });
 
-    return { api, getUser }
-}
+  const getUser = async () => {
+    try {
+      const response = await api.get("/user");
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-export { useApi }
+  return { api, getUser };
+};
+
+export { useApi };
