89c7523eab585d04cf854e942ddcb04627e54823
gustavosilva7 <gustavosilvaalexgusta007@gmail.com>
2024-06-22 23:01:45 -0300

    wip

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,10 +5,41 @@ import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { Link } from "expo-router";
-import { useAuth } from "@/app/AuthProvider";
+import { useEffect, useState } from "react";
+import { useApi } from "@/hooks/useApi";
+import List from "@/components/List";
 
 export default function HomeScreen({ navigation }: any) {
-  const { currentUser } = useAuth();
+  const [myBooks, setMyBooks] = useState<Books[]>([]);
+  const [myWishList, setMyWishList] = useState<Books[]>([]);
+  const { api } = useApi();
+
+  const fetchMyBooks = async () => {
+    try {
+      const { data } = await api.get("/students/books-reads");
+
+      setMyBooks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchWishlist = async () => {
+    try {
+      const { data } = await api.get("/wish-list");
+
+      setMyWishList(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMyBooks();
+
+    fetchWishlist();
+  }, []);
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -19,18 +50,30 @@ export default function HomeScreen({ navigation }: any) {
         />
       }
     >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome, {currentUser.name}!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-
-      <ThemedView>
-        <Pressable
-          onPress={() => navigation.navigate("modal")}
-          style={{ padding: 16 }}
+      <ThemedView style={styles.listsContainer}>
+        <ThemedText
+          style={{
+            fontSize: 24,
+            fontWeight: "bold",
+            marginTop: 20,
+          }}
         >
-          <ThemedText>Go to modal</ThemedText>
-        </Pressable>
+          Livros que já li
+        </ThemedText>
+        <List books={myBooks} lending navigation={navigation} />
+      </ThemedView>
+      <ThemedView style={styles.listsContainer}>
+        <ThemedText
+          style={{
+            fontSize: 24,
+            fontWeight: "bold",
+            marginTop: 20,
+          }}
+        >
+          Lista de próximos a serem lidos
+        </ThemedText>
+        <List books={myWishList} navigation={navigation} />
+
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -38,8 +81,8 @@ export default function HomeScreen({ navigation }: any) {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
+
+    flexDirection: "column",
     gap: 8,
   },
   stepContainer: {
@@ -50,7 +93,12 @@ const styles = StyleSheet.create({
     height: 120,
     width: 370,
     bottom: 0,
-    left: 20,
-    position: "absolute",
+    left: 0,
+    position: 'absolute',
+  },
+  listsContainer: {
+    flexDirection: "column",
+    gap: 8,
+    height: 290,
   },
 });
